2bd4fd7d299e3d4fa7a0f6451a84dae459b53fa2
majorek31 <m4y0r.offical@gmail.com>
2022-05-20 18:20:52 +0200

    Create api's error handling

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const prisma = require('@prisma/client')
+const fs = require('fs')
 
 router.get('/info', (req, res) => {
-
+    if (!req.query.id)
+        return res.json({
+            status: 1,
+            data: {
+                message: "You must provide a file's id in order to retrive info about it."
+            }
+        })
 })
 
+//TODO: return data about size of stored file, number of them, etc
 router.get('/status', (req, res) => {
 
 })
 
 router.post('/upload', (req, res) => {
+    if (!req.body.file)
+        return res.json({
+            status: 2,
+            data: {
+                message: "You must provide a file in order to upload it."
+            }
+        })
     
 })
 
