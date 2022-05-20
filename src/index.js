2884b7dcdc5657197c7b76a8b8cfeb2ecf80b41f
majorek31 <m4y0r.offical@gmail.com>
2022-05-20 18:15:41 +0200

    Add body-parser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.use('/api/', require('./api/index'))
 
 app.listen(8080, (err) => {
