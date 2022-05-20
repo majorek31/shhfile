d05d8a7f8fdf3788fe0d7ead5db4036e975b5648
majorek31 <m4y0r.offical@gmail.com>
2022-05-20 18:01:09 +0200

    Create basic call handler

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/info', (req, res) => {
+
+})
+
+router.get('/status', (req, res) => {
+
+})
+
+router.post('/upload', (req, res) => {
+    
+})
+
+module.exports = router
