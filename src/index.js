5ff5852406f370498e0a91afa2791462977a1056
majorek31 <m4y0r.offical@gmail.com>
2022-05-20 18:25:09 +0200

    Add express-fileupload middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fileupload = require('express-fileupload')
 const app = express();
 
+app.use(fileupload())
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.use('/api/', require('./api/index'))
 
 app.listen(8080, (err) => {
