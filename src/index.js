141ed7d96780da3ebef6f578939ed3838e1b5cef
majorek31 <m4y0r.offical@gmail.com>
2022-05-21 22:41:54 +0200

    Add routing to specified file

--- src/index.js
+++ src/index.js
@@ -20,6 +20,20 @@ app.use(bodyparser.urlencoded({extended: true}))
 app.use('/api/', require('./api/index'))
 app.use('/files/', require('./files/index'))
 
+if (config.routing.enabled){
+    try {
+        fs.mkdirSync(path.join(__dirname, '../public'))
+    } catch (err) {
+        if (err)
+         if (!err.code === "EEXIST")
+            throw err
+    }
+    app.use('/', express.static(path.join(__dirname, '../public')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, `../public/${config.routing.index}`))
+    })
+}
+
 app.listen(config.port, (err) => {
     if (err) throw err;
     console.log(`Server started listening on port: ${config.port}`)
